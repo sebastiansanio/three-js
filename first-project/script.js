f0f361e58e47a1e2319ebf4f50090dcd3f9b8471
Sebastian Sanio <sebastiansanio@outlook.com>
2024-11-29 13:27:48 -0500

    feat: add more things to first project

--- first-project/script.js
+++ first-project/script.js
@@ -10,7 +10,16 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 'red'})
 
 const mesh = new THREE.Mesh(geometry, material)
+// Position
+mesh.position.x = 1
+// Scale
+mesh.scale.x = 2
+// Rotation
+mesh.rotation.y = Math.PI * 0.25
 scene.add(mesh)
+
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
 
 const sizes = {
     width: 800,
@@ -19,6 +28,7 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 
 scene.add(camera)
 
